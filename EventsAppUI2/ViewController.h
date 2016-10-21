76b17160193b593534cbbafdd822b7e5f63a70d9
Jonathan Villegas <jonathanvillegas@jonathans-macbook-pro.local>
2016-10-21 18:25:49 +0800

    Initial Commit

---
+++ EventsAppUI2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EventsAppUI2
+//
+//  Created by Jonathan Villegas on 21/10/2016.
+//  Copyright © 2016 Jonathan Villegas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ EventsAppUI2/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  EventsAppUI2
+//
+//  Created by Jonathan Villegas on 21/10/2016.
+//  Copyright © 2016 Jonathan Villegas. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
